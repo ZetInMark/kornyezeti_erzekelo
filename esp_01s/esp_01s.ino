7169f851d7a65f28bad65829ad06e971feda7929
Hunor1167 <kovacshunor02@gmail.com>
2023-01-07 22:55:34 +0100

    az esp01 kodja

--- esp_01s/esp_01s.ino
+++ esp_01s/esp_01s.ino
@@ -38,6 +38,58 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while(Serial.available() > 0) //ha vannak byteok a bufferben akkor....
+  {
+    //Helyet csinálunk az érkező üzenetnek
+    //egy karakter tömböt hozunk létre amiben majd tároljuk az egyes karaktereket
+    static unsigned int uzenet_poz = 0;  //ezt fogjuk változtatni hogy lépkedjünk a buffer elemei között.
+
+    //Beolvassuk a következő byte-ot a soros monitorról
+    char inByte = Serial.read();
+
+    //Ellenőrizzük a bejövő üzenetet hogy biztos nem terminating character e (Ez dönti el hogy a buffer / üzenet végén vagyunk e)
+
+    if(inByte != '\n')  //ellenőrizzük hogy ez nem egy new line terminating character // && (uzenet_poz < MAX_UZENET_HOSSZ - 1
+    {
+      //Hozzáadjuk a bejövő byte-ot a karakter tömbünkhöz
+      uzenet[uzenet_poz] = inByte;
+      uzenet_poz++;
+    }
+
+    //Az else ág akkor valósul meg ha az egész üzenetet befogadtuk
+    else
+    {
+      //adjunk egy null karaktert a stringnek
+      uzenet[uzenet_poz] = '\0';  //ez fontos mivel ha atoi() átalakító használunk ez kell a végére
+
+      //Írjuk ki az üzenetet vagy csináljunk vele amit szeretnénk
+
+
+
+      byte index = 0;
+      ptr = strtok(uzenet, ",");  // szeparátor karakter
+      while (ptr != NULL)
+      {
+        strings[index] = ptr;
+        index++;
+        ptr = strtok(NULL, ",");
+      }
+
+      String paratartalom = strings[0];
+      String homerseklet = strings[1];
+      String talajnedv = strings[2];
+
+
+      //átadjuk a webhooknak ami elküldi a http kérést
+      webhook.trigger(paratartalom,homerseklet,talajnedv);
+
+
+      //reseteljuk az uzenet poziciot
+
+      uzenet_poz = 0;
+}
+
+  }
+
 
 }
