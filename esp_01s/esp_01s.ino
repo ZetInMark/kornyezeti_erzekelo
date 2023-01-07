087fa5b4b76a2970a5b608b035efc402d5be6d73
Hunor1167 <kovacshunor02@gmail.com>
2023-01-07 22:51:22 +0100

    az esp01 kodja

--- esp_01s/esp_01s.ino
+++ esp_01s/esp_01s.ino
@@ -1,3 +1,24 @@
+#include <ESP8266Webhook.h>
+#include <ESP8266WiFi.h>
+
+#define _SSID "Hunor"      //WiFi SSID
+#define _PASSWORD "Asdfghj11"  //WiFi Password
+#define KEY "jAy-d9XRNZtCrslkWWC0u3EUBGBFzswAJLdhjUJVDOU"        // Webhook kulcs
+#define EVENT "tweet"      // Webhook Event Name
+
+Webhook webhook(KEY, EVENT);    // Create an object.
+
+const unsigned int MAX_UZENET_HOSSZ = 17;
+static char uzenet[MAX_UZENET_HOSSZ];
+
+char strings[3]; // elemek száma amit át szeretnék adni a webhooknak
+charptr = NULL;
+
+unsigned long seconds = 1000L;
+unsigned long minutes = seconds * 180; //3 perc varakozasi ido, ez a long kell mivel az int az csak 32000 milisecondig megy
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
